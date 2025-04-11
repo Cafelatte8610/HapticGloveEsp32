d5e2e4583b26cad6af4087349c76a1473fd752ed
Cafelatte8610 <58741134+Cafelatte8610@users.noreply.github.com>
2025-04-12 08:07:29 +0900

    InitiArduinoIO

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+#define LED_PIN 14
+#define IO_PIN 26
+int initAngle = 2500;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(100);
+  Serial.printf("%s - run\n", __func__);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(IO_PIN) - initAngle;
+  Serial.println(sensorValue);
+  delay(100);
+}
